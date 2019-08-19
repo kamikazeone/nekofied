e34204ed573dfcf527af5cc0e360f739d747fdbb
Ajuandp <49325037+andreasjp0@users.noreply.github.com>
2019-08-19 19:52:31 +0700

    Yeeee Sembunyi

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -138,7 +138,7 @@ else:
     # pylint: disable=invalid-name
     bot = TelegramClient("userbot", API_KEY, API_HASH)
 
-TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)i
+TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
 
 # Global Variables
 HELPER = {}
